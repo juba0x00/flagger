c87b8805b8afdf8c66f39e5e30802783d7ae05f6
juba0x00 <mahmoudhawash90@gmail.com>
2022-12-04 19:53:55 +0200

    adding base16, base32, base85

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Strings-Resolver
-Search any flag format in `strings` output 
+Search any flag format in `strings` output
 
+![demo](./demo.png)
 
 ## Todo List 
 
 - [x] Search for the plain-text flag 
-- [ ] Search for the Base64 of the flag (base64, 42, ...)
+- [ ] Search for the bases of the flag (base64, 42, ...)
   - [x] base64 
-  - base42
+  - [ ] base42
+  - [x] base16 (hex)
+  - [ ] base8 (octal)
+  - [ ] base2 (binary)
+  - [x] base32
+  - [x] base85
 - [ ] Search for the ROT13 of the flag (ROT1, ROT2, ...)
--- strings-resolver.py
+++ strings-resolver.py
@@ -5,12 +5,11 @@ from colorama import Fore
 import base64
 # Forensics Strings resolver
 
-
 def parse_arguments():
     parser = ArgumentParser(description='Search for the flag in strings output')
-    parser.add_argument('flag_format', help='Specify beginning of flag format (Ex: TUCTF)')
-    parser.add_argument('file_name', help='Specify the file name')
-    parser.add_argument('-d', '--debug', action='store_true', help='Don\'t ask for input and Auto fill them') # this option is used to make the testing easier
+    parser.add_argument('-f', '--flag-format', help='Specify beginning of flag format (Ex: TUCTF)')
+    parser.add_argument('-n', '--file-name', help='Specify the file name')
+    parser.add_argument('-d', '--debug', action='store_true', help='Don\'t ask for input and Auto fill them')  # this option is used to make the testing easier
     return parser.parse_args()
 
 
@@ -21,13 +20,42 @@ def check_plain_flag(data):
             return True
 
 
+def check_base16_flag(data):
+    for line in data:
+        base16_flag = base64.b16encode(flag_format.encode('ascii')).decode('ascii')
+
+        if line.startswith(base16_flag):
+            print(f"{Fore.RED}{line} -> {base64.b16decode(line.encode('ascii')).decode('ascii')}{Fore.RESET}")
+            return True
+
+
+def check_base32_flag(data):
+    for line in data:
+        base32_flag = base64.b32encode(flag_format.encode('ascii')).decode('ascii').replace('=', '')
+        base32_flag = base32_flag[:len(base32_flag) - 1]  # to avoid the last digit unmatching
+
+        if line.startswith(base32_flag):
+            print(f"{Fore.RED}{line} -> {base64.b32decode(line.encode('ascii')).decode('ascii')}{Fore.RESET}")
+            return True
+
+
 def check_base64_flag(data):
     for line in data:
         base64_flag = base64.b64encode(flag_format.encode('ascii')).decode('ascii').replace('=', '')
-        base64_flag = base64_flag[:len(base64_flag)-1] # to avoid the last digit unmatching
+        base64_flag = base64_flag[:len(base64_flag)-1]  # to avoid the last digit unmatching
 
         if line.startswith(base64_flag):
             print(f"{Fore.RED}{line} -> {base64.b64decode(line.encode('ascii')).decode('ascii')}{Fore.RESET}")
+            return True
+
+
+def check_base85_flag(data):
+    for line in data:
+        base85_flag = base64.b85encode(flag_format.encode('ascii')).decode('ascii').replace('=', '')
+        base85_flag = base85_flag[:len(base85_flag)-1]  # to avoid the last digit unmatching
+
+        if line.startswith(base85_flag):
+            print(f"{Fore.RED}{line} -> {base64.b85decode(line.encode('ascii')).decode('ascii')}{Fore.RESET}")
             return True
 
 
@@ -44,4 +72,7 @@ if __name__ == '__main__':
     lines = strings_output.split('\n')
 
     print(f'{Fore.GREEN}no plain-text flag found{Fore.RESET}') if not check_plain_flag(lines[:]) else None
+    print(f'{Fore.GREEN}no base16 (hexadecimal) flag found{Fore.RESET}') if not check_base16_flag(lines[:]) else None
+    print(f'{Fore.GREEN}no base32 flag found{Fore.RESET}') if not check_base32_flag(lines[:]) else None
     print(f'{Fore.GREEN}no base64 flag found{Fore.RESET}') if not check_base64_flag(lines[:]) else None
+    print(f'{Fore.GREEN}no base85 flag found{Fore.RESET}') if not check_base85_flag(lines[:]) else None
