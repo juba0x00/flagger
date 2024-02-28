4323428fb8c5e74c6023c4a5d19df809f26ef87e
juba0x00 <mahmoud.31057455@ics.tanta.edu.eg>
2024-02-29 00:08:49 +0200

    refactor oct_encode()

--- modules/oct.py
+++ modules/oct.py
@@ -1,33 +1,8 @@
-#!/usr/bin/env python3
+def oct_encode(plain: str) -> str:
+    return ''.join([oct(ord(char))[2:] for char in plain])
 
-'''
-OCTAL ENCODER/DECODER
 
-encode: str --> ascii --> oct
-decode: oct --> ascii --> str
-
-example1: '124125103124106' <--> 'TUCTF'
-example2: '124125103124106173164150151163137151163137157143164141154175' <--> TUCTF{this_is_octal}
-'''
-
-#---- ENCODE ----#
-def oct_encode(word):
-    #string to ascii
-    word_ascii = []
-    for i in word: word_ascii.append(ord(i))
-
-    #ascii to oct
-    word_oct = ''
-    for i in word_ascii: word_oct += oct(i)
-
-    #remove leading 0s
-    word_oct = word_oct.split('0o')
-    word_oct = ''.join(word_oct)
-
-    return word_oct
-
-#---- DECODE ----# 
-def oct_decode(word):
+def oct_decode(word: str) -> str:
     #oct to ascii 
     oct_to_ascii = []
     for i in range(0, len(word), 3): oct_to_ascii.append(str(int(word[i:i+3],8)))
@@ -37,7 +12,3 @@ def oct_decode(word):
     for i in oct_to_ascii: ascii_to_str += chr(int(i))
 
     return ascii_to_str
-
-# --- TEST --- #
-#print(oct_encode('TUCTF'))
-#print(oct_decode('124125103124106173164150151163137151163137157143164141154175'))
